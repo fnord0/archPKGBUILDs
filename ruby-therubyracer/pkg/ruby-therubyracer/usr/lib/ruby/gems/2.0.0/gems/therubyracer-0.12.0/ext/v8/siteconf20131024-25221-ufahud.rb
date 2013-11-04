cf96795d9ef59239e828bf028ed1d1f85d25936c
fnord0 <fnord0@riseup.net>
2013-11-04 09:30:18 -0800

    updated a slew of AUR packages, to get everything current

---
+++ ruby-therubyracer/pkg/ruby-therubyracer/usr/lib/ruby/gems/2.0.0/gems/therubyracer-0.12.0/ext/v8/siteconf20131024-25221-ufahud.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20131024-25221-1yafzh2"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
